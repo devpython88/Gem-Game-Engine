73e7686384da2c01401ddfed54d041827653b8c3
UniDev69 <devpython88@gmail.com>
2025-08-26 19:42:26 +0530

    Delete build directory

--- build/CMakeFiles/edit_cache.dir/utility.sh
+++
@@ -1,4 +0,0 @@
-set -e
-
-cd /c/Users/Sadik/Documents/roger/build
-/usr/bin/ccmake.exe -S$(CMAKE_SOURCE_DIR) -B$(CMAKE_BINARY_DIR)
--- build/CMakeFiles/rebuild_cache.dir/utility.sh
+++
@@ -1,4 +0,0 @@
-set -e
-
-cd /c/Users/Sadik/Documents/roger/build
-/usr/bin/cmake.exe --regenerate-during-build -S$(CMAKE_SOURCE_DIR) -B$(CMAKE_BINARY_DIR)
